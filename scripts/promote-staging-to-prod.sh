15114b95d03a6d3f52e2421d7a282cf250ff7c5c
tonybergeron <tonybergeron@gmail.com>
2016-05-25 14:56:09 -0500

    updates to scripts for example to promote

--- scripts/promote-staging-to-prod.sh
+++ scripts/promote-staging-to-prod.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 # Run from within the scripts folder as sudo (Sudo allows heroku scripts to operate correctly)
-# sudo ./promote-staging-to-demo.sh
+# sudo ./promote-staging-to-prod.sh
 
 sh ./promote-setup.sh
 
 # Heroku Pipelines promote from Staging to Production
 # heroku plugins:install heroku-pipelines
-if heroku pipelines:promote --app smrt-staging --to smrt-production ; then
-    echo "Success: Heroku promotion of smrt-staging to smrt-production"
+if heroku pipelines:promote --app app-staging --to app-prod ; then
+    echo "Success: Heroku promotion of app-staging to app-prod"
 else
-    echo "Failure: Heroku promotion of smrt-staging to smrt-production"
+    echo "Failure: Heroku promotion of app-staging to app-prod"
 fi
 
 # Upload Build Files to S3
 # Have awscli installed and configured with a user that has IAM Permissions of `AmazonS3FullAccess`
-if aws s3 rm s3://smrtcloud-production-build --recursive && aws s3 sync s3://smrtcloud-staging-build s3://smrtcloud-production-build --acl public-read --cache-control "public, max-age=86400" ; then
-    echo "Success: AWS S3 Deployment of smrtcloud-staging-build to smrtcloud-production-build"
+if aws s3 rm s3://app-prod-build --recursive && aws s3 sync s3://app-staging-build s3://app-prod-build --acl public-read --cache-control "public, max-age=86400" ; then
+    echo "Success: AWS S3 Deployment of app-staging-build to app-prod-build"
 else
-    echo "Failure: AWS S3 Deployment of smrtcloud-staging-build to smrtcloud-production-build"
+    echo "Failure: AWS S3 Deployment of app-staging-build to app-prod-build"
 fi
